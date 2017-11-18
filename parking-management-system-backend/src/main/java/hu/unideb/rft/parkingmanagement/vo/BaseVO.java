fccf1481c6de505fd81adb39bf3da46a9253475a
miklos.becsei <miklos.becsei@sightspot.hu>
2017-11-18 13:08:51 +0100

    BaseVO created

---
+++ parking-management-system-backend/src/main/java/hu/unideb/rft/parkingmanagement/vo/BaseVO.java
@@ -0,0 +1,13 @@
+package hu.unideb.rft.parkingmanagement.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+public class BaseVO implements Serializable {
+
+    private Long id;
+}
